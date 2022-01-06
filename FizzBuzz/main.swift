5d4ccedd20feda7edaa6b6c4db5ce83a5bfb36d0
Randi <randimaulanaakbar@gmail.com>
2022-01-06 20:12:41 +0700

    first commit

--- FizzBuzz/main.swift
+++ FizzBuzz/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+var oneThousandNumbers = [Int] ()
+
+for i in 1...1000 {
+    oneThousandNumbers.append(i)
+}
+
+for numb in oneThousandNumbers {
+    if numb % 3 == 0 && numb % 5 == 0  {
+        print("\(numb) FizzBuzz")
+    } else if numb % 3 == 0 {
+        print("\(numb)  Fizz")
+    } else if numb % 5 == 0 {
+        print("\(numb) Buzz")
+    } else {
+        print(numb)
+    }
+}
 
